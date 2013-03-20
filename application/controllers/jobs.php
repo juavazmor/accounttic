1155d19a0fb5f8d2deedf3ac93d550acfe38c22e
Daniel Entrenas <decemuladores@gmail.com>
2013-03-20 18:45:19 +0100

    Jobs terminado

--- application/controllers/jobs.php
+++ application/controllers/jobs.php
@@ -35,6 +35,20 @@ class Jobs_Controller extends Base_Controller
 		return Redirect::to_route('jobs');
 	}
 
+	public function get_edit( $id ) {
+		$clients = Client::get();
+		$job = Job::find($id);
+
+		if (!$job)	return Rediret::to('jobs');
+
+		return View::make('job.edit')
+				->with(array(
+					"old_inputs" => null,
+					"clients" => $clients,
+					"job" => $job
+					));
+	}
+
 	public function post_create() {
 
 		$job_name	 = Input::get('name');
@@ -69,5 +83,42 @@ class Jobs_Controller extends Base_Controller
 		));
 
 		return Redirect::to_route("jobs");
+	}
+
+	public function put_update($id) {
+
+		$job = Job::find($id);
+
+		$job_name	 = Input::get('name');
+		$client_id 	 = Input::get('client');
+		$amount		 = Input::get('amount');
+		$finished	 = (Input::get('finished') == 1) ? 1 : 0;
+		$deadline 	 = new DateTime(Input::get('deadline'));
+
+		$validation  = Job::validate_post( array(
+				'name' 	 	=> $job_name,
+				'amount' 	=> $amount,
+				'deadline' 	=> $deadline) 
+		);
+
+        if ( $validation !== false ) {
+
+			return View::make('job.new')
+					->with(array(
+						'old_inputs' => Input::all(),
+						'clients' 	 => $clients
+						)
+					)
+					->with_errors($validation->errors);
+        }
+
+        $job->name = $job_name;
+        $job->client_id = $client_id;
+        $job->amount = $amount;
+        $job->finished = $finished;
+        $job->deadline =$deadline;
+
+        if ( $job->save() )
+            return Redirect::to_route('jobs');
 	}
 }
---
+++ application/views/job/edit.blade.php
@@ -0,0 +1,45 @@
+@layout('master')
+
+
+@section('heading')
+	<h2>Edit job</h2>
+@endsection
+
+@section('container')
+	<?php
+		foreach ($clients as $client) 
+		{
+			$clients_array[$client->id] = $client->name;
+		}
+
+		$date = new DateTime($job->deadline);
+
+	?>
+
+	{{ Form::open('/jobs/' . $job->id, 'PUT') }}
+		{{ Form::label('name', 'Name of the job') }}
+		{{ Form::text('name', $job->name) }}
+		<div class="control-group{{ $errors->has('name') ? ' error' : '' }}">
+			<p class=control-label>{{ $errors->first('name') }}</p>
+		</div>
+		{{ Form::label('client', 'Client') }}
+		{{ Form::select('client', $clients_array, $job->client_id) }}
+		{{ Form::label('amount', 'Amount of the job') }}
+		{{ Form::text('amount', $job->amount) }}<span class="currency">€</span>
+		<div class="control-group{{ $errors->has('amount') ? ' error' : '' }}">
+			<p class=control-label>{{ $errors->first('amount') }}</p>
+		</div>
+		{{ Form::label('deadline', 'Deadline') }}
+		<div class="input-append date" id="dp3" data-date="{{ date("d/m/Y") }}" data-date-format="dd/mm/yyyy">
+			{{ Form::text('deadline', $date->format("d/m/Y"), array("class" => "span2 big", "id" => "dp1") ) }}
+		<span class="add-on"><i class="icon-calendar"></i></span>
+		</div>
+		{{ Form::label('finished', 'Finished?')}}
+		{{ Form::checkbox('finished') }}
+		<div class="topmargin">
+			{{ Form::submit('Edit', array("class" => "btn btn-success")) }}
+			{{ HTML::link_to_route('jobs', 'Cancel', '', array('class' => 'btn btn-warning') ) }}
+		</div>
+		
+	{{ Form::close() }}
+@endsection
